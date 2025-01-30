b25b9fe315c47b3d04aa97fc27501d5582dd39fc
Matteo Carnevale Schianca <135057045+matteocarnevale@users.noreply.github.com>
2025-01-30 15:20:57 +0100

    Add files via upload

---
+++ models/archs/__init__.py
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from BasicSR (https://github.com/xinntao/BasicSR)
+# Copyright 2018-2020 BasicSR Authors
+# ------------------------------------------------------------------------
+
+from .arch_util import *
+from .Baseline_arch import *
+from .local_arch import *
+from .NAFNet_arch import *
+from .NAFSSR_arch import *
